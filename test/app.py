d12cb3f5f9c5e9061b75593ad305f948f369e841
abspen1 <abspencer2097@gmail.com>
2020-09-03 13:05:49 -0700

    add testing for webapp

---
+++ test/app.py
@@ -0,0 +1,34 @@
+import tweepy
+from textblob import TextBlob
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import redis
+import seaborn as sns
+import schedule
+import time
+import re
+import os
+
+
+consumer_key = os.getenv("CONSUMER_KEY")
+consumer_secret = os.getenv("CONSUMER_SECRET")
+key = os.getenv("KEY")
+secret = os.getenv("SECRET")
+client = redis.Redis(host="10.10.10.1", port=6379, db=0,
+                     password=os.getenv("REDIS_PASS"))
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(key, secret)
+auth.secure = True
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+png_file = "/tmp/plot.png"
+
+
+def main():
+    acct = api.get_user("interntendie")
+    print(acct.followers_count)
+    print(acct.favourites_count)
+    print(acct.statuses_count)
+
+if __name__ == "__main__":
+    main()
