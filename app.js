3f35eed68754e0439e9e2485d9326ba41ec10fc6
abspen1 <abspencer2097@gmail.com>
2020-09-03 13:42:35 -0700

    backend working

--- app.js
+++ app.js
@@ -11,17 +11,21 @@ const getTwitterData = async () => {
     try {
         const response = await axios.get(`${BASE_URL}/austinapi/tendie-intern`);
 
-        followers_span.innerHTML = response.data.Followers
-        tweets_liked_span.innerHTML = response.data.TweetsLiked
+        followers_span.innerHTML = numberWithCommas(response.data.Followers)
+        tweets_liked_span.innerHTML = numberWithCommas(response.data.TweetsLiked)
 
-        tweets_span.innerHTML = response.data.Tweets
-        tweets_read_span.innerHTML = response.data.TweetsRead
+        tweets_span.innerHTML = numberWithCommas(response.data.Tweets)
+        tweets_read_span.innerHTML = numberWithCommas(response.data.TweetsRead)
 
         console.log(response)
     } catch (e) {
         console.error(e);
     }
 };
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 
 // This is to set up our existing projects on reload of site
 function main() {
